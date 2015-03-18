f829fb6d51dd783974802c9cf5e1bc7bdd3247ef
mbdavid <mauricio.david@gmail.com>
2015-03-18 14:27:24 -0300

    Update version to 1.0

--- LiteDB/Properties/AssemblyInfo.cs
+++ LiteDB/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("54989b5c-4bcf-4d58-b8ba-9b014a324f76")]
 
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("UnitTest")]
