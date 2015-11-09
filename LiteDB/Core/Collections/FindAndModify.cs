fd399307016a36315d4e34efb8ba02427c77e1c7
mbdavid <mauricio.david@gmail.com>
2015-11-09 16:26:03 -0200

    Added FindAndModify

--- LiteDB/Core/Collections/FindAndModify.cs
+++ LiteDB/Core/Collections/FindAndModify.cs
@@ -7,8 +7,42 @@ using System.Text;
 
 namespace LiteDB
 {
-    // Execute find operation in index only - do not deserialize documents
     public partial class LiteCollection<T>
     {
+        /// <summary>
+        /// Query documents and execute, for each document, action method. All data is locked during execution
+        /// </summary>
+        public void FindAndModify(Query query, Action<T> action)
+        {
+            if (query == null) throw new ArgumentNullException("query");
+            if (action == null) throw new ArgumentNullException("action");
+
+            this.Database.Transaction.Begin();
+
+            try
+            {
+                var docs = this.Find(query);
+
+                foreach(var doc in docs)
+                {
+                    action(doc);
+                }
+
+                this.Database.Transaction.Commit();
+            }
+            catch
+            {
+                this.Database.Transaction.Rollback();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Query documents and execute, for each document, action method. All data is locked during execution
+        /// </summary>
+        public void FindAndModify(Expression<Func<T, bool>> predicate, Action<T> action)
+        {
+            this.FindAndModify(_visitor.Visit(predicate), action);
+        }
     }
 }
