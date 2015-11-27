1284cde23cb7abb59e76f9ec5b77d0d5326208bb
mbdavid <mauricio.david@gmail.com>
2015-11-26 23:14:30 -0200

    Fix names

--- LiteDB/Shell/Commands/Collections/Read.cs
+++ LiteDB/Shell/Commands/Collections/Read.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if DEBUG
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -6,11 +7,12 @@ using System.Text;
 
 namespace LiteDB.Shell.Commands
 {
-    internal class CollectionFind : BaseCollection, ILiteCommand
+    // to test only - only read all data
+    internal class CollectionRead : BaseCollection, ILiteCommand
     {
         public bool IsCommand(StringScanner s)
         {
-            return this.IsCollectionCommand(s, "find");
+            return this.IsCollectionCommand(s, "read");
         }
 
         public BsonValue Execute(LiteDatabase db, StringScanner s)
@@ -19,8 +21,15 @@ namespace LiteDB.Shell.Commands
             var query = this.ReadQuery(s);
             var skipLimit = this.ReadSkipLimit(s);
             var docs = col.Find(query, skipLimit.Key, skipLimit.Value);
+            var cnt = 0;
 
-            return new BsonArray(docs);
+            foreach(var d in docs)
+            {
+                cnt++;
+            }
+
+            return new BsonValue(cnt);
         }
     }
 }
+#endif
