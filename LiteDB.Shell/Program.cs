2489b5e8eba09688f740f8a54cc67fccdbdc25d7
mbdavid <mauricio.david@gmail.com>
2015-12-01 17:59:52 -0200

    Shell options

--- LiteDB.Shell/Program.cs
+++ LiteDB.Shell/Program.cs
@@ -11,52 +11,18 @@ namespace LiteDB.Shell
     {
         static void Main(string[] args)
         {
-            LiteDatabase db = null;
-            var input = new InputCommand();
-            var display = new Display();
+            var o = new OptionSet(args);
 
-            display.TextWriters.Add(Console.Out);
-
-            // show welcome message
-            display.WriteWelcome();
-
-            // if has a argument, its database file - try open
-            if (args.Length > 0)
+            if(o.Upgrade != null)
             {
-                try
-                {
-                    db = new LiteDatabase(args[0]);
-                }
-                catch (Exception ex)
-                {
-                    display.WriteError(ex.Message);
-                }
+                // do upgrade
             }
-
-            while (true)
+            else if(o.Run != null)
             {
-                // read next command from user
-                var cmd = input.ReadCommand();
-
-                if (string.IsNullOrEmpty(cmd)) continue;
-
-                try
-                {
-                    var isConsoleCommand = ConsoleCommand.TryExecute(cmd, ref db, display, input);
-
-                    if (isConsoleCommand == false)
-                    {
-                        if(db == null) throw LiteException.NoDatabase();
-
-                        var result = db.Run(cmd);
-
-                        display.WriteResult(result);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    display.WriteError(ex.Message);
-                }
+            }
+            else
+            {
+                ShellProgram.Start(o.Filename);
             }
         }
     }
---
+++ LiteDB.Shell/Utils/Options.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LiteDB.Shell
+{
+    public class OptionSet
+    {
+        public string Filename;
+        public string Upgrade;
+        public string Run;
+
+        public OptionSet(string[] args)
+        {
+            var options = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var index = 0;
+
+            foreach (var item in args)
+            {
+                var m = Regex.Match(item, @"^(--|-|\/)(\w+)[=:]?(.*)$");
+
+                if (m.Success)
+                {
+                    var key = m.Groups[2].Value;
+                    var value = m.Groups[3].Value;
+
+                    options.Add(key, string.IsNullOrWhiteSpace(value) ? "true" : value);
+                }
+                else if (index == 0)
+                {
+                    this.Filename = item;
+                }
+
+                index++;
+            }
+
+            options.TryGetValue("upgrade", out this.Upgrade);
+            options.TryGetValue("run", out this.Run);
+        }
+    }
+}
