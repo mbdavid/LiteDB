1b678ad1a4853d10d23be5d507de1a2b8d16ee9d
mbdavid <mauricio.david@gmail.com>
2016-09-24 21:56:14 -0300

    Bug in recovery

--- LiteDB.Tests/Engine/CacheTest.cs
+++ LiteDB.Tests/Engine/CacheTest.cs
@@ -12,6 +12,8 @@ namespace LiteDB.Tests
     [TestClass]
     public class CacheTest
     {
+        const int N = 60000;
+
         [TestMethod]
         public void CacheCheckpoint_Test()
         {
@@ -22,14 +24,66 @@ namespace LiteDB.Tests
                 db.Log.Level = Logger.CACHE;
                 db.Log.Logging += (s) => log.AppendLine(s);
 
-                // insert basic 200.000 documents
-                db.Insert("col", GetDocs(200000));
+                // insert basic N documents
+                db.Insert("col", GetDocs(N));
 
                 Assert.IsTrue(log.ToString().Contains("checkpoint"));
             }
         }
 
-        private IEnumerable<BsonDocument> GetDocs(int count)
+        [TestMethod]
+        public void Recovery_Test()
+        {
+            using (var file = new TempFile())
+            {
+                // init with N docs
+                using (var db = new LiteEngine(file.Filename))
+                {
+                    db.EnsureIndex("col", "type");
+
+                    // insert N docs with type=1
+                    db.Insert("col", GetDocs(N, 1, false));
+
+                    // checks count
+                    Assert.AreEqual(N, db.Count("col", Query.EQ("type", 1)));
+                }
+
+                // re-open and type update
+                using (var db = new LiteEngine(file.Filename))
+                {
+                    var log = new StringBuilder();
+                    db.Log.Level = Logger.CACHE;
+                    db.Log.Logging += (s) => log.AppendLine(s);
+
+                    try
+                    {
+                        // try update all to "type=2"
+                        // but throws before finish
+                        db.Update("col", GetDocs(N, 2, false));
+                    }
+                    catch (Exception ex)
+                    {
+                        if (!ex.Message.Contains("Try Recovery!")) Assert.Fail(ex.Message);
+                    }
+
+                    // checks if cache had a checkpoint
+                    Assert.IsTrue(log.ToString().Contains("checkpoint"));
+
+                    // re-check if all docs will be type=1
+                    Assert.AreEqual(0, db.Count("col", Query.EQ("type", 1)));
+                    Assert.AreEqual(N, db.Count("col", Query.EQ("type", 2)));
+                }
+
+                // re-open datafile the be sure contains only type=1
+                using (var db = new LiteEngine(file.Filename))
+                {
+                    Assert.AreEqual(0, db.Count("col", Query.EQ("type", 1)));
+                    Assert.AreEqual(N, db.Count("col", Query.EQ("type", 2)));
+                }
+            }
+        }
+
+        private IEnumerable<BsonDocument> GetDocs(int count, int type = 1, bool throwAtEnd = false)
         {
             for(var i = 0; i < count; i++)
             {
@@ -37,8 +91,15 @@ namespace LiteDB.Tests
                 {
                     { "_id", i },
                     { "name", Guid.NewGuid().ToString() },
+                    { "type", type },
                     { "lorem", TempFile.LoremIpsum(3, 5, 2, 3, 3) }
                 };
+            }
+
+            // force throw exception during big insert
+            if (throwAtEnd)
+            {
+                throw new Exception("Try Recovery!");
             }
         }
     }
