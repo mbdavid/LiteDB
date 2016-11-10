2ad553e3b73931c828e66e3dcebb1e7aa622554d
nerai <dev@kikashi.net>
2016-11-11 00:23:28 +0100

    formatting

--- LiteDB/DbEngine/Disks/FileDiskService.cs
+++ LiteDB/DbEngine/Disks/FileDiskService.cs
@@ -387,11 +387,11 @@ namespace LiteDB
                 }
                 catch (UnauthorizedAccessException)
                 {
-                        LitePlatform.Platform.WaitFor (10);
+                    LitePlatform.Platform.WaitFor (10);
                 }
                 catch (IOException ex)
                 {
-                        ex.WaitIfLocked (10);
+                    ex.WaitIfLocked (10);
                 }
             }
 
