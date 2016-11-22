e05aa8942d464c0db45fea9044ef79bc3ab36968
mbdavid <mauricio.david@gmail.com>
2016-11-22 08:04:12 -0200

    Add Upsert to LiteCollection #94

---
+++ LiteDB/Database/Collections/Upsert.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LiteDB
+{
+    public partial class LiteCollection<T>
+    {
+        /// <summary>
+        /// Insert or Update a document in this collection. Returns false if not found document in collection
+        /// </summary>
+        public bool Upsert(T document)
+        {
+            if (document == null) throw new ArgumentNullException("document");
+
+            // get BsonDocument from object
+            var doc = _mapper.ToDocument(document);
+
+            return _engine.Value.Upsert(_name, doc);
+        }
+
+        /// <summary>
+        /// Insert or Update a document in this collection. Returns false if not found document in collection
+        /// </summary>
+        public bool Upsert(BsonValue id, T document)
+        {
+            if (document == null) throw new ArgumentNullException("document");
+            if (id == null || id.IsNull) throw new ArgumentNullException("id");
+
+            // get BsonDocument from object
+            var doc = _mapper.ToDocument(document);
+
+            // set document _id using id parameter
+            doc["_id"] = id;
+
+            return _engine.Value.Upsert(_name, doc);
+        }
+
+        /// <summary>
+        /// Insert or Update all documents
+        /// </summary>
+        public int Upsert(IEnumerable<T> documents)
+        {
+            if (documents == null) throw new ArgumentNullException("document");
+
+            return _engine.Value.Upsert(_name, documents.Select(x => _mapper.ToDocument(x)));
+        }
+    }
+}
