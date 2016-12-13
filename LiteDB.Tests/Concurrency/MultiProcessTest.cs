3161e217c100957238527c1e86451c8917bac6a7
mbdavid <mauricio.david@gmail.com>
2016-12-12 22:29:57 -0200

    Add more multi process unit test #377

--- LiteDB.Tests/Concurrency/MultiProcessTest.cs
+++ LiteDB.Tests/Concurrency/MultiProcessTest.cs
@@ -51,5 +51,87 @@ namespace LiteDB.Tests
                 }
             }
         }
+
+        [TestMethod]
+        public void MultiProcess_InsertQuery_Test()
+        {
+            using (var file = new TempFile())
+            {
+                using (var dbA = new LiteEngine(file.Filename))
+                using (var dbB = new LiteEngine(file.Filename))
+                {
+                    dbA.EnsureIndex("col", "process", false);
+
+                    // insert 1000 x instance=1
+                    var ta = Task.Factory.StartNew(() =>
+                    {
+                        for (var i = 0; i < 1000; i++)
+                        {
+                            dbA.Insert("col", new BsonDocument { { "process", 1 } });
+                        }
+                    });
+
+                    // keep quering until found 1000 docs
+                    var tb = Task.Factory.StartNew(() =>
+                    {
+                        var count = 0L;
+
+                        while (count < 1000)
+                        {
+                            // force query all rows
+                            count = dbB.Count("col", Query.EQ("process", 1));
+
+                            Task.Delay(50).Wait();
+                        }
+                    });
+
+                    Task.WaitAll(ta, tb);
+
+                    Assert.AreEqual(1000, dbA.Count("col", Query.EQ("process", 1)));
+                    Assert.AreEqual(1000, dbB.Count("col", Query.EQ("process", 1)));
+
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MultiProcess_InsertDelete_Test()
+        {
+            using (var file = new TempFile())
+            {
+                using (var dbA = new LiteEngine(file.Filename))
+                using (var dbB = new LiteEngine(file.Filename))
+                {
+                    dbA.EnsureIndex("col", "process", false);
+
+                    // insert 1000 x instance=1
+                    var ta = Task.Factory.StartNew(() =>
+                    {
+                        for (var i = 0; i < 1000; i++)
+                        {
+                            dbA.Insert("col", new BsonDocument { { "process", 1 } });
+                        }
+                    });
+
+                    // keeping delete all
+                    var tb = Task.Factory.StartNew(() =>
+                    {
+                        Task.Delay(500).Wait();
+
+                        while (dbB.Count("col", Query.EQ("process", 1)) > 0)
+                        {
+                            dbB.Delete("col", Query.All());
+                            Task.Delay(50).Wait();
+                        }
+                    });
+
+                    Task.WaitAll(ta, tb);
+
+                    Assert.AreEqual(0, dbA.Count("col", Query.EQ("process", 1)));
+                    Assert.AreEqual(0, dbB.Count("col", Query.EQ("process", 1)));
+
+                }
+            }
+        }
     }
 }
