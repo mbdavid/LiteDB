96b2bdba617bcf21100cd2903f13eb26499e5152
andypelzer <andy.pelzer@gmx.de>
2016-12-17 00:20:42 +0100

    Added a test that reproduces my problem / the NullReferenceException

---
+++ LiteDB.Tests/Database/NullRefTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiteDB.Tests.Database
+{
+    public class Pipeline
+    {
+        public int Id { get; set; }
+        public List<Job> Jobs { get; set; } = new List<Job>();
+    }
+
+    public class Job
+    {
+        public int Id { get; set; }
+    }
+
+    [TestClass]
+    public class NullRefTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void DbRef_ToDeleted_ThrowsNullReferenceException()
+        {
+            var mapper = new BsonMapper();
+            mapper.Entity<Pipeline>().DbRef(x => x.Jobs, "jobs");
+
+            using (var db = new LiteDatabase(new MemoryStream(), mapper))
+            {
+                var pipelineCollection = db.GetCollection<Pipeline>("pipelines");
+                var jobCollection = db.GetCollection<Job>("jobs");
+
+                var pipeline = new Pipeline();
+                pipelineCollection.Insert(pipeline);
+
+                var job = new Job();
+                jobCollection.Insert(job);
+
+                pipeline.Jobs.Add(job);
+                pipelineCollection.Update(pipeline);
+
+                jobCollection.Delete(job.Id);
+
+                var thisWillThrow = db.GetCollection<Pipeline>("pipelines").Include(p => p.Jobs).FindAll().ToArray();
+            }
+        }
+    }
+}
