1e515f9d1becd654c8436c4ee8cf3d707d0214c7
mbdavid <mauricio.david@gmail.com>
2017-02-04 14:58:35 -0200

    Add more examples to Implicit test #453

--- LiteDB.Tests/Document/ImplicitTest.cs
+++ LiteDB.Tests/Document/ImplicitTest.cs
@@ -15,20 +15,21 @@ namespace LiteDB.Tests
         [TestMethod]
         public void Implicit_Test()
         {
-            int i = 10;
-            long l = 20;
-            ulong ul = 99;
+            int i = int.MaxValue;
+            long l = long.MaxValue;
+            ulong u = ulong.MaxValue;
 
             BsonValue bi = i;
             BsonValue bl = l;
-            BsonValue bu = ul;
+            BsonValue bu = u;
 
             Assert.IsTrue(bi.IsInt32);
             Assert.IsTrue(bl.IsInt64);
             Assert.IsTrue(bu.IsDouble);
 
-
-
+            Assert.AreEqual(i, bi.AsInt32);
+            Assert.AreEqual(l, bl.AsInt64);
+            Assert.AreEqual(u, bu.AsDouble);
         }
     }
 }
