7363ce793a082602f992992203f6676277cc7327
mbdavid <mauricio.david@gmail.com>
2017-08-16 20:24:27 -0300

    Change version to 3.1.4

--- LiteDB.Shell/Properties/AssemblyInfo.cs
+++ LiteDB.Shell/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("01ce385b-31a7-4b1a-9487-23fe8acb3888")]
-[assembly: AssemblyVersion("3.1.3.0")]
-[assembly: AssemblyFileVersion("3.1.3.0")]
+[assembly: AssemblyVersion("3.1.4.0")]
+[assembly: AssemblyFileVersion("3.1.4.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
