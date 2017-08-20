bd81ad011ce9c940614358552cd536a98355cfed
mbdavid <mauricio.david@gmail.com>
2017-08-20 08:46:47 -0300

    Update version to 4

--- LiteDB.Shell/Properties/AssemblyInfo.cs
+++ LiteDB.Shell/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("01ce385b-31a7-4b1a-9487-23fe8acb3888")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
