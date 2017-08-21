f5283b71cf1b2b14c5c41c63feab157f1ea5f081
mbdavid <mauricio.david@gmail.com>
2017-08-21 08:52:40 -0300

    Support for int/long/guid/date autoId in shell using id:type

--- LiteDB/Shell/Collections/Insert.cs
+++ LiteDB/Shell/Collections/Insert.cs
@@ -14,17 +14,23 @@ namespace LiteDB.Shell
         public IEnumerable<BsonValue> Execute(StringScanner s, LiteEngine engine)
         {
             var col = this.ReadCollection(engine, s);
-            var value = JsonSerializer.Deserialize(s.ToString());
+            var value = JsonSerializer.Deserialize(s);
+            var sid = s.Scan(@"\s+_?id:(int32|int64|int|long|objectid|datetime|date|guid)", 1).Trim().ToLower();
+            var autoId =
+                sid == "int32" || sid == "int" ? BsonType.Int32 :
+                sid == "int64" || sid == "long" ? BsonType.Int64 :
+                sid == "date" || sid == "datetime" ? BsonType.DateTime :
+                sid == "guid" ? BsonType.Guid : BsonType.ObjectId;
 
             if (value.IsArray)
             {
-                var count = engine.Insert(col, value.AsArray.RawValue.Select(x => x.AsDocument));
+                var count = engine.Insert(col, value.AsArray.RawValue.Select(x => x.AsDocument), autoId);
 
                 yield return count;
             }
             else
             {
-                engine.Insert(col, new BsonDocument[] { value.AsDocument });
+                engine.Insert(col, new BsonDocument[] { value.AsDocument }, autoId);
 
                 yield return value.AsDocument["_id"];
             }
