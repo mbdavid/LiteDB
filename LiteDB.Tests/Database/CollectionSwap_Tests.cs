d7a4be6b6b0b9bb80860bb17380a5695a9ab73e9
Bustux <Bustux.hd@gmail.com>
2017-09-11 23:33:09 +0200

    Add Collection Swapping

---
+++ LiteDB.Tests/Database/CollectionSwap_Tests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiteDB.Tests.Database
+{
+    [TestClass]
+    public class CollectionSwap_Tests
+    {
+        private const string Collectioname = "Hempels";
+        private class A
+        {
+            public string Name { get; set; }
+        }
+
+        private class B
+        {
+            public string Element { get; set; }
+        }
+
+        [TestMethod, TestCategory("Database")]
+        public void Collection_Swap()
+        {
+            using (var db = new LiteDatabase(@"demo.db"))
+            {
+                // Preparing
+                var col = db.GetCollection<A>(Collectioname);
+                col.Insert(new BsonValue("A"),new A {Name = "A"});
+                var col2 = db.GetCollection<B>(Collectioname);
+                var testElement = new B {Element = "B"};
+                col2.Insert(new BsonValue("B"), testElement);
+
+                var col3 = col.Swap<B>();
+                var belement = col3.FindById(new BsonValue("B"));
+                Assert.AreEqual(testElement.Element, belement.Element);
+            }
+        }
+    }
+}
