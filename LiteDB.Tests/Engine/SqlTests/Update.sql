d2398db946a87d4014ad4372e616caaf6de1dacf
mbdavid <mauricio.david@gmail.com>
2018-06-28 13:33:19 -0300

    Add new sql unit test way to test - using output parameters

---
+++ LiteDB.Tests/Engine/SqlTests/Update.sql
@@ -0,0 +1,9 @@
+﻿--
+-- SQL Tests
+--
+-- All command will be run at once, so always use ; after each command
+-- If want test command result use "SET @par = [command]" to get result into "par" output parameter
+
+SET @count1 = UPDATE person SET { age: 99 } WHERE _id = 1;
+
+SET @newAge = SELECT age FROM person WHERE _id = 1;
--- LiteDB.Tests/Engine/Sql_Tests.cs
+++ LiteDB.Tests/Engine/Sql_Tests.cs
@@ -17,172 +17,66 @@ namespace LiteDB.Tests.Engine
     [TestClass]
     public class Sql_Tests
     {
-        #region Initialize
+        #region Run Tests
 
-        private LiteEngine db;
-        private BsonDocument[] person;
-
-        [TestInitialize]
-        public void Init()
+        public BsonDocument Run(string name, Action<LiteEngine> setup = null)
         {
-            db = new LiteEngine();
-            person = DataGen.Person(1, 100).ToArray();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"LiteDB.Tests.Engine.SqlTests.{name}.sql");
+            var reader = new StreamReader(stream);
+            var sql = reader.ReadToEnd();
 
-            db.Insert("person", person);
-        }
+            var par = new BsonDocument();
 
-        [TestCleanup]
-        public void CleanUp()
-        {
-            db.Dispose();
-        }
-
-        [DebuggerHidden]
-        private void Invalid(string sql)
-        {
-            try
+            using (var db = new LiteEngine())
             {
-                db.Execute(sql);
+                setup?.Invoke(db);
 
-                Assert.Fail("Must fail with this syntax: " + sql);
+                try
+                {
+                    using (var data = db.Execute(sql, par))
+                    {
+                        while (data.NextResult()) ;
+                    }
+                }
+                catch(LiteException ex) when (ex.ErrorCode == LiteException.UNEXPECTED_TOKEN)
+                {
+                    var p = (int)ex.Position;
+                    var start = (int)Math.Max(p - 30, 1) - 1;
+                    var end = Math.Min(p + 15, sql.Length);
+                    var length = end - start;
+                    var str = sql.Substring(start, length).Replace('\n', ' ').Replace('\r', ' ');
+
+                    Assert.Fail($"{ex.Message} - {str}");
+                }
             }
-            catch (LiteException ex) when (ex.ErrorCode == LiteException.UNEXPECTED_TOKEN) { }
+
+            return par;
         }
 
         #endregion
 
         [TestMethod]
-        public void Sql_Insert_Single()
+        public void Sql_Insert_Commands()
         {
-            Assert.AreEqual(1, db.Execute("INSERT INTO col1 VALUES {a:1}").Current.AsInt32);
+            var output = this.Run("Insert");
+
+            Assert.AreEqual(1, output["insert1"].AsInt32, "Insert into single document");
+            Assert.AreEqual(3, output["insert3"].AsInt32, "Insert into multi document");
+
+            Assert.AreEqual(1, output["int"][0].AsInt32, "AutoId using Int32");
+            Assert.AreEqual(1, output["long"][0].AsInt32, "AutoId using Long");
+            Assert.AreEqual(1, output["date"][0].AsInt32, "AutoId using Date");
+            Assert.AreEqual(1, output["guid"][0].AsInt32, "AutoId using Guid");
+            Assert.AreEqual(1, output["objectid"][0].AsInt32, "AutoId using ObjectId");
         }
 
         [TestMethod]
-        public void Sql_Insert_Bulk()
+        public void Sql_Update_Commands()
         {
-            Assert.AreEqual(3, db.Execute("INSERT INTO col1 VALUES {a:1}, {a:2}, {a:3}").Current.AsInt32);
+            var output = this.Run("Update", db => db.Insert("person", DataGen.Person(1, 10)));
+
+            Assert.AreEqual(1, output["count1"].AsInt32, "Update single document");
+            Assert.AreEqual(99, output["newAge"][0].AsInt32, "Document was updated");
         }
-
-        [TestMethod]
-        public void Sql_Insert_With_Custom_Id()
-        {
-            db.Execute("INSERT INTO col2 VALUES {a:25}, {a:26}, {a:27} WITH ID=INT");
-
-            Assert.AreEqual(25, db.FindById("col2", 1)["a"].AsInt32);
-        }
-
-        [TestMethod]
-        public void Sql_Invalid_Commands()
-        {
-            Invalid("INSERT col2 VALUES {a:1}");
-            Invalid("INSERT INTO col2 {a:1}");
-            Invalid("INSERT INTO col2 VALUES {a:1} WITH ID-INT");
-            Invalid("INSERT INTO col-2 VALUES {a:1}");
-            Invalid("INSERT INTO col2 VALUES true");
-            Invalid("INSERT INTO col2 VALUES {a:1} WITH ID=INT 9");
-        }
-
-        [TestMethod]
-        public void Sql_Update_Add_Field_For_All()
-        {
-            db.Execute("UPDATE person SET { x: 1 }");
-
-            Assert.AreEqual(person.Length, db.Count("person", "x = 1"));
-        }
-
-        [TestMethod]
-        public void Sql_Update_Change_Field_With_Where()
-        {
-            Assert.AreEqual(0, db.Count("person", "name = UPPER(name) AND _id = 1"));
-
-            db.Execute("UPDATE person SET { name: UPPER(name) } WHERE _id > 0 AND age > 0");
-
-            Assert.AreEqual(1, db.Count("person", "name = UPPER(name) AND _id = 1"));
-        }
-
-        [TestMethod]
-        public void Sql_Replace_Single_Document()
-        {
-            db.Execute("REPLACE person SET { _id, name, age: 10 } WHERE _id = 25");
-
-            var d = db.FindById("person", 25);
-
-            Assert.AreEqual(3, d.Keys.Count);
-            Assert.AreEqual(10, d["age"].AsInt32);
-        }
-
-        [TestMethod]
-        public void Sql_Analyze()
-        {
-            Assert.AreEqual(0, db.Execute("ANALYZE").Current.AsInt32);
-        }
-
-        [TestMethod]
-        public void Sql_Vaccum()
-        {
-            Assert.AreEqual(0, db.Execute("VACCUM").Current.AsInt32);
-        }
-
-        [TestMethod]
-        public void Sql_Checkpoint()
-        {
-            Assert.AreEqual(0, db.Execute("CHECKPOINT").Current.AsInt32);
-        }
-
-        [TestMethod]
-        public void Sql_Transaction()
-        {
-            var t = db.Execute("BEGIN").Current.AsGuid;
-            Assert.AreEqual(t, db.Execute("BEGIN TRANS").Current.AsGuid);
-
-            Assert.IsTrue(db.Execute("COMMIT").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("COMMIT TRANS").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("COMMIT TRANSACTION").Current.AsBoolean);
-
-            Assert.AreNotEqual(t, db.Execute("BEGIN TRANSACTION").Current.AsGuid);
-
-            Assert.IsTrue(db.Execute("ROLLBACK").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("ROLLBACK TRANS").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("ROLLBACK TRANSACTION").Current.AsBoolean);
-        }
-
-        [TestMethod]
-        public void Sql_Create_Drop_Index()
-        {
-            Assert.IsTrue(db.Execute("CREATE INDEX idx1 ON person ( UPPER($.city) );").Current.AsBoolean);
-
-            Assert.IsTrue(db.Execute("CREATE UNIQUE INDEX idx2 ON person (email)").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("CREATE UNIQUE INDEX idx2 ON person ($.email)").Current.AsBoolean);
-
-            Assert.IsTrue(db.Execute("DROP INDEX person.idx1;").Current.AsBoolean);
-            Assert.IsTrue(db.Execute("DROP INDEX person.idx2;").Current.AsBoolean);
-        }
-
-        [TestMethod]
-        public void Sql_Drop_Collection()
-        {
-            db.Execute("INSERT INTO ncol VALUES {a:1};");
-
-            Assert.IsTrue(db.Execute("DROP COLLECTION ncol;").Current.AsBoolean);
-            Assert.IsFalse(db.Execute("DROP COLLECTION ncol;").Current.AsBoolean);
-        }
-
-        [TestMethod]
-        public void Sql_Multi_Commands()
-        {
-            var r = db.Execute("INSERT INTO m0 VALUES {a:1}; INSERT INTO m1 VALUES {a:1}; DROP COLLECTION m0;");
-
-            while (r.NextResult()) ;
-
-            Assert.IsTrue(db.GetCollectionNames().Any(x => x == "m0"));
-        }
-
-        [TestMethod]
-        public void Sql_Select_Simple()
-        {
-            var r = db.Execute("SELECT $ FROM person");
-        }
-
-
     }
 }
