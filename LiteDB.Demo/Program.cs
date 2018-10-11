00df88645effe031d32b9558562697ca15b4a750
mbdavid <mauricio.david@gmail.com>
2018-10-10 23:06:05 -0300

    Adding UI for connection string

--- LiteDB.Demo/Program.cs
+++ LiteDB.Demo/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using LiteDB;
 
 namespace LiteDB.Demo
@@ -12,33 +14,40 @@ namespace LiteDB.Demo
         [STAThread]
         static void Main(string[] args)
         {
-            var orig = @"C:\Temp\service.c.db";
-
-            var docs = JsonSerializer.DeserializeArray("").Select(x => x.AsDocument);
+            var orig = @"C:\Git\LiteDB\LiteDB.Shell\bin\Debug\net40\teste.db";
 
 
-            var report = LiteEngine.Recovery(orig);
 
-            Console.WriteLine("Recovery Report:\n" + report);
+            using (var db = new LiteEngine(orig))
+            {
+                var col = db.GetCollectionNames().First();
 
-            //using (var db = new LiteEngine(@"C:\Temp\SessionDatabase-recovery.ldb"))
-            //{
-            //    // reading all database
-            //    foreach (var col in db.GetCollectionNames())
-            //    {
-            //        Console.WriteLine("Collection: " + col);
-            //    
-            //    
-            //        foreach (var doc in db.Find(col, Query.All("Token.LastUsedOn")).Take(10))
-            //        {
-            //            Console.WriteLine(JsonSerializer.Serialize(doc, true));
-            //            // ok
-            //        }
-            //    }
-            //}
+                var tt = db.FindAll(col).ToArray();
+
+                var c = db.FindOne("DataObjects_1", Query.EQ("_id", "qyeyeW.1oMJK5"));
+
+                
+
+                Console.WriteLine(c == null);
+
+            }
+                //{
+                //    // reading all database
+                //    foreach (var col in db.GetCollectionNames())
+                //    {
+                //        Console.WriteLine("Collection: " + col);
+                //    
+                //    
+                //        foreach (var doc in db.Find(col, Query.All("Token.LastUsedOn")).Take(10))
+                //        {
+                //            Console.WriteLine(JsonSerializer.Serialize(doc, true));
+                //            // ok
+                //        }
+                //    }
+                //}
 
 
-            Console.WriteLine("End.");
+                Console.WriteLine("End.");
             Console.ReadKey();
         }
     }
