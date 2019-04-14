c2c5d8af4aef43e19dcad8d6fc0d0b5204cce8a2
Jens Schadron <jens.schadron@3factr.be>
2019-04-15 00:15:14 +0200

    Added query benchmarks

---
+++ LiteDB.Benchmarks/Benchmarks/Queries/QueryIndexBenchmarks.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using LiteDB.Benchmarks.Models;
+using LiteDB.Benchmarks.Models.Generators;
+
+namespace LiteDB.Benchmarks.Benchmarks.Queries
+{
+    public class QueryIndexBenchmarks : BenchmarkBase
+    {
+        protected override string DatabasePath => @"Query.db";
+
+        private LiteDatabase DatabaseInstance { get; set; }
+        private LiteCollection<FileMetaBase> _fileMetaCollection { get; set; }
+
+        [GlobalSetup(Targets = new[] {nameof(FindWithExpression), nameof(FindWithQuery)})]
+        public void GlobalSetup()
+        {
+            DatabaseInstance = new LiteDatabase(new ConnectionString(DatabasePath) {Mode = FileMode.Exclusive});
+            _fileMetaCollection = DatabaseInstance.GetCollection<FileMetaBase>();
+
+            _fileMetaCollection.Insert(FileMetaGenerator<FileMetaBase>.GenerateList(N)); // executed once per each N value
+        }
+
+        [GlobalSetup(Targets = new[] {nameof(FindWithIndexExpression), nameof(FindWithIndexQuery)})]
+        public void GlobalIndexSetup()
+        {
+            DatabaseInstance = new LiteDatabase(new ConnectionString(DatabasePath) {Mode = FileMode.Exclusive});
+            _fileMetaCollection = DatabaseInstance.GetCollection<FileMetaBase>();
+            _fileMetaCollection.EnsureIndex(fileMeta => fileMeta.IsFavorite);
+            _fileMetaCollection.EnsureIndex(fileMeta => fileMeta.ShouldBeShown);
+
+            _fileMetaCollection.Insert(FileMetaGenerator<FileMetaBase>.GenerateList(N)); // executed once per each N value
+        }
+
+        [Benchmark(Baseline = true)]
+        public List<FileMetaBase> FindWithExpression()
+        {
+            return _fileMetaCollection.Find(fileMeta => fileMeta.IsFavorite).ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> FindWithQuery()
+        {
+            return _fileMetaCollection.Find(Query.EQ(nameof(FileMetaBase.IsFavorite), true)).ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> FindWithIndexExpression()
+        {
+            return _fileMetaCollection.Find(fileMeta => fileMeta.IsFavorite).ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> FindWithIndexQuery()
+        {
+            return _fileMetaCollection.Find(Query.EQ(nameof(FileMetaBase.IsFavorite), true)).ToList();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            // Disposing logic
+            DatabaseInstance.DropCollection(nameof(FileMetaBase));
+            DatabaseInstance.Dispose();
+        }
+    }
+}
---
+++ LiteDB.Benchmarks/Benchmarks/Queries/QueryMultipleParametersBenchmark.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using LiteDB.Benchmarks.Models;
+using LiteDB.Benchmarks.Models.Generators;
+
+namespace LiteDB.Benchmarks.Benchmarks.Queries
+{
+    public class QueryMultipleParametersBenchmark : BenchmarkBase
+    {
+        protected override string DatabasePath => @"Query.db";
+
+        private LiteDatabase DatabaseInstance { get; set; }
+        private LiteCollection<FileMetaBase> _fileMetaCollection { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            DatabaseInstance = new LiteDatabase(new ConnectionString(DatabasePath) {Mode = FileMode.Exclusive});
+            _fileMetaCollection = DatabaseInstance.GetCollection<FileMetaBase>();
+            _fileMetaCollection.EnsureIndex(fileMeta => fileMeta.IsFavorite);
+            _fileMetaCollection.EnsureIndex(fileMeta => fileMeta.ShouldBeShown);
+
+            _fileMetaCollection.Insert(FileMetaGenerator<FileMetaBase>.GenerateList(N)); // executed once per each N value
+        }
+
+        [Benchmark(Baseline = true)]
+        public List<FileMetaBase> Expression_Normal_Baseline()
+        {
+            return _fileMetaCollection.Find(fileMeta => fileMeta.IsFavorite && fileMeta.ShouldBeShown).ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> Query_Normal()
+        {
+            return _fileMetaCollection.Find(Query.And(
+                    Query.EQ(nameof(FileMetaBase.IsFavorite), true),
+                    Query.EQ(nameof(FileMetaBase.ShouldBeShown), true)))
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> Expression_ParametersSwitched()
+        {
+            return _fileMetaCollection.Find(fileMeta => fileMeta.ShouldBeShown && fileMeta.IsFavorite).ToList();
+        }
+
+        [Benchmark]
+        public List<FileMetaBase> Query_ParametersSwitched()
+        {
+            return _fileMetaCollection.Find(Query.And(
+                    Query.EQ(nameof(FileMetaBase.ShouldBeShown), true),
+                    Query.EQ(nameof(FileMetaBase.IsFavorite), true)))
+                .ToList();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            // Disposing logic
+            DatabaseInstance.DropCollection(nameof(FileMetaBase));
+            DatabaseInstance.Dispose();
+        }
+    }
+}
