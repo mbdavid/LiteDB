7ef823a2d0301069f492ff481d968dce74b403bd
Jens Schadron <jens.schadron@3factr.be>
2019-04-15 00:14:57 +0200

    Added insertion benchmarks

--- LiteDB.Benchmarks/Benchmarks/BenchmarkBase.cs
+++ LiteDB.Benchmarks/Benchmarks/BenchmarkBase.cs
@@ -4,6 +4,8 @@ namespace LiteDB.Benchmarks.Benchmarks
 {
     public abstract class BenchmarkBase
     {
+        protected virtual string DatabasePath { get; }
+
         [Params(10, 50, 100, 500, 1000, 5000, 10000)]
         public int N;
     }
