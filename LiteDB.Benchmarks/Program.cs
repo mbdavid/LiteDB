31e73ce69b027ad8ff9cc1d6255834417dc3e508
Jens Schadron <jens.schadron@3factr.be>
2019-05-21 17:16:11 +0200

    Updated runtime versions to .NET Core 2.2 and Mono 5.18 Cleaned up entrypoint

--- LiteDB.Benchmarks/Program.cs
+++ LiteDB.Benchmarks/Program.cs
@@ -2,7 +2,6 @@
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Filters;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.CsProj;
@@ -11,42 +10,17 @@ namespace LiteDB.Benchmarks
 {
     class Program
     {
-        // sudo mono LiteDB.Benchmarks/bin/Release/net471/LiteDB.Benchmarks.exe
+        // sudo mono LiteDB.Benchmarks/bin/Release/net472/LiteDB.Benchmarks.exe
         static void Main(string[] args)
         {
-            /*BenchmarkRunner.Run(typeof(Program).Assembly, DefaultConfig.Instance
-                .With(MemoryDiagnoser.Default)
-                .With(BenchmarkReportExporter.Default, HtmlExporter.Default, MarkdownExporter.GitHub)
-                .With(Job.Mono
-                    .With(Jit.Llvm)
-                    .With(new[] {new MonoArgument("--optimize=inline")})
-                    .WithGcForce(true))
-                .With(Job.Core
-                    .With(Jit.RyuJit)
-                    .With(CsProjCoreToolchain.NetCoreApp21)
-                    .WithGcForce(true)));*/
-
-            /*BenchmarkRunner.Run<Benchmarks.Queries.QueryIgnoreExpressionPropertiesBenchmark>(DefaultConfig.Instance
-                .With(MemoryDiagnoser.Default)
-                .With(BenchmarkReportExporter.Default, HtmlExporter.Default, MarkdownExporter.GitHub)
-                .With(Job.Mono
-                    .With(Jit.Llvm)
-                    .With(new[] {new MonoArgument("--optimize=inline")})
-                    .WithGcForce(true))
-                .With(Job.Core
-                    .With(Jit.RyuJit)
-                    .With(CsProjCoreToolchain.NetCoreApp21)
-                    .WithGcForce(true)));*/
-
             BenchmarkRunner.Run(typeof(Program).Assembly, DefaultConfig.Instance
-                //.With(new AnyCategoriesFilter(new[] {Benchmarks.Constants.Categories.QUERIES}))
                 .With(Job.Mono
                     .With(Jit.Llvm)
                     .With(new[] {new MonoArgument("--optimize=inline")})
                     .WithGcForce(true))
                 .With(Job.Core
                     .With(Jit.RyuJit)
-                    .With(CsProjCoreToolchain.NetCoreApp21)
+                    .With(CsProjCoreToolchain.NetCoreApp22)
                     .WithGcForce(true))
                 .With(MemoryDiagnoser.Default)
                 .With(BenchmarkReportExporter.Default, HtmlExporter.Default, MarkdownExporter.GitHub)
