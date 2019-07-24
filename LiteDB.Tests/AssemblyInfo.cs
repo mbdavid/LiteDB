2ae2fa3a639d7323bdb6e98a83d4496f65e78bbd
Jens Schadron <jens.schadron@3factr.be>
2019-07-25 00:30:54 +0200

    Test

---
+++ LiteDB.Tests/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+namespace LiteDB.Tests
+{
+    public class AssemblyInfo
+    {
+    }
+}
--- LiteDB.Tests/Utils/Broken_Tests.cs
+++ LiteDB.Tests/Utils/Broken_Tests.cs
@@ -4,7 +4,7 @@ namespace LiteDB.Tests
 {
     public class Broken_Tests
     {
-        [Fact(Skip = "Throws exception (obviously)")]
+        [Fact]
         public void Throw_Exception()
         {
             throw new System.Exception("Exception from forced-throwing test");
