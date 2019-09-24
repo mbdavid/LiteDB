7936b869f0fcdd5d1145b869c96f68f217a24a83
mbdavid <mauricio.david@gmail.com>
2019-09-24 16:33:24 -0300

    Fix Query helper class AND/OR #1320

--- LiteDB.Tests/Query/QueryApi_Tests.cs
+++ LiteDB.Tests/Query/QueryApi_Tests.cs
@@ -7,7 +7,7 @@ namespace LiteDB.Tests.QueryTest
 {
     public class QueryApi_Tests : Person_Tests
     {
-        [Fact(Skip = "must fix multi expressions parameters")]
+        [Fact]
         public void Query_And()
         {
             var r0 = local.Where(x => x.Age == 22 && x.Active == true).ToArray();
@@ -15,7 +15,16 @@ namespace LiteDB.Tests.QueryTest
             var r1 = collection.Find(Query.And(Query.EQ("Age", 22), Query.EQ("Active", true))).ToArray();
 
             AssertEx.ArrayEqual(r0, r1, true);
+        }
 
+        [Fact]
+        public void Query_And_Same_Field()
+        {
+            var r0 = local.Where(x => x.Age > 22 && x.Age < 25).ToArray();
+
+            var r1 = collection.Find(Query.And(Query.GT("Age", 22), Query.LT("Age", 25))).ToArray();
+
+            AssertEx.ArrayEqual(r0, r1, true);
         }
     }
 }
