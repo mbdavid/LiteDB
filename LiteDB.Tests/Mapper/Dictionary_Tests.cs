9867b393de09850b722cc2cf5d830c0a2b56b9fa
mbdavid <mauricio.david@gmail.com>
2019-10-22 10:35:39 -0300

    Fixing Dictionary with datetime key #1350

---
+++ LiteDB.Tests/Mapper/Dictionary_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace LiteDB.Tests.Mapper
+{
+    public class Dictionary_Tests
+    {
+        public class Dict
+        {
+            public IDictionary<DateTime, string> DateDict { get; set; } = new Dictionary<DateTime, string>();
+        }
+
+        private BsonMapper _mapper = new BsonMapper();
+
+        [Fact]
+        public void Dictionary_Map()
+        {
+            var obj = new Dict();
+
+            obj.DateDict[DateTime.Now] = "now!";
+
+            var doc = _mapper.ToDocument(obj);
+
+            var newobj = _mapper.ToObject<Dict>(doc);
+
+            newobj.DateDict.Keys.First().Should().Be(obj.DateDict.Keys.First());
+        }
+    }
+}
