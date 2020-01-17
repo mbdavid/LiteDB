c378aa0b32843f1d4ad311846be9d71b5869af8d
mbdavid <mauricio.david@gmail.com>
2020-01-17 15:59:52 -0300

    Remove $pragmas and fix $database - update to rc version

--- LiteDB/Engine/SystemCollections/SysDatabase.cs
+++ LiteDB/Engine/SystemCollections/SysDatabase.cs
@@ -10,32 +10,47 @@ namespace LiteDB.Engine
     {
         private IEnumerable<BsonDocument> SysDatabase()
         {
-            yield return new BsonDocument { ["_id"] = "name", ["value"] = _disk.GetName(FileOrigin.Data) };
-            yield return new BsonDocument { ["_id"] = "encrypted", ["value"] = _settings.Password != null };
-            yield return new BsonDocument { ["_id"] = "readOnly", ["value"] = _settings.ReadOnly };
-            yield return new BsonDocument { ["_id"] = "lastPageID", ["value"] = (int)_header.LastPageID };
-            yield return new BsonDocument { ["_id"] = "freeEmptyPageID", ["value"] = (int)_header.FreeEmptyPageList };
-            yield return new BsonDocument { ["_id"] = "creationTime", ["value"] = _header.CreationTime };
-            yield return new BsonDocument { ["_id"] = "dataFileSize", ["value"] = (int)_disk.GetLength(FileOrigin.Data) };
-            yield return new BsonDocument { ["_id"] = "logFileSize", ["value"] = (int)_disk.GetLength(FileOrigin.Log) };
-            yield return new BsonDocument { ["_id"] = "asyncQueueLength", ["value"] = _disk.Queue.Length };
-            yield return new BsonDocument { ["_id"] = "currentReadVersion", ["value"] = _walIndex.CurrentReadVersion };
-            yield return new BsonDocument { ["_id"] = "lastTransactionID", ["value"] = _walIndex.LastTransactionID };
-            yield return new BsonDocument { ["_id"] = "extendSegments", ["value"] = _disk.Cache.ExtendSegments };
             yield return new BsonDocument
             {
-                ["_id"] = "memoryUsage",
-                ["value"] = (_disk.Cache.ExtendSegments * MEMORY_SEGMENT_SIZE * PAGE_SIZE) +
-                                                    (40 * (_disk.Cache.ExtendSegments * MEMORY_SEGMENT_SIZE))
+                ["name"] = _disk.GetName(FileOrigin.Data),
+                ["encrypted"] = _settings.Password != null,
+                ["readOnly"] = _settings.ReadOnly,
+
+                ["lastPageID"] = (int)_header.LastPageID,
+                ["freeEmptyPageID"] = (int)_header.FreeEmptyPageList,
+
+                ["creationTime"] = _header.CreationTime,
+
+                ["dataFileSize"] = (int)_disk.GetLength(FileOrigin.Data),
+                ["logFileSize"] = (int)_disk.GetLength(FileOrigin.Log),
+                ["asyncQueueLength"] = _disk.Queue.Length,
+
+                ["currentReadVersion"] = _walIndex.CurrentReadVersion,
+                ["lastTransactionID"] = _walIndex.LastTransactionID,
+
+                ["pragmas"] = new BsonDocument(_header.Pragmas.Pragmas.ToDictionary(x => x.Name, x => x.Get())),
+
+                ["cache"] = new BsonDocument
+                {
+                    ["extendSegments"] = _disk.Cache.ExtendSegments,
+                    ["memoryUsage"] =
+                    (_disk.Cache.ExtendSegments * MEMORY_SEGMENT_SIZE * PAGE_SIZE) +
+                    (40 * (_disk.Cache.ExtendSegments * MEMORY_SEGMENT_SIZE)),
+                    ["freePages"] = _disk.Cache.FreePages,
+                    ["readablePages"] = _disk.Cache.GetPages().Count,
+                    ["writablePages"] = _disk.Cache.WritablePages,
+                    ["pagesInUse"] = _disk.Cache.PagesInUse,
+                },
+
+                ["transactions"] = new BsonDocument
+                {
+                    ["open"] = _monitor.Transactions.Count,
+                    ["maxOpenTransactions"] = MAX_OPEN_TRANSACTIONS,
+                    ["initialTransactionSize"] = _monitor.InitialSize,
+                    ["availableSize"] = _monitor.FreePages
+                }
+
             };
-            yield return new BsonDocument { ["_id"] = "freePages", ["value"] = _disk.Cache.FreePages };
-            yield return new BsonDocument { ["_id"] = "readablePages", ["value"] = _disk.Cache.GetPages().Count };
-            yield return new BsonDocument { ["_id"] = "writablePages", ["value"] = _disk.Cache.WritablePages };
-            yield return new BsonDocument { ["_id"] = "pagesInUse", ["value"] = _disk.Cache.PagesInUse };
-            yield return new BsonDocument { ["_id"] = "openTransactions", ["value"] = _monitor.Transactions.Count };
-            yield return new BsonDocument { ["_id"] = "maxOpenTransactions", ["value"] = MAX_OPEN_TRANSACTIONS };
-            yield return new BsonDocument { ["_id"] = "initialTransactionSize", ["value"] = _monitor.InitialSize };
-            yield return new BsonDocument { ["_id"] = "availableTransactionSize", ["value"] = _monitor.FreePages };
         }
     }
 }
