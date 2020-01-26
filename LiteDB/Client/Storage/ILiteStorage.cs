4444df31aa80116060f3455063fb54ca7e04b024
klettier <lettier.kevin@gmail.com>
2020-01-26 20:55:19 +0100

    Reapply naming of T to TFileId

--- LiteDB/Client/Storage/ILiteStorage.cs
+++ LiteDB/Client/Storage/ILiteStorage.cs
@@ -5,81 +5,81 @@ using System.Linq.Expressions;
 
 namespace LiteDB
 {
-    public interface ILiteStorage<T>
+    public interface ILiteStorage<TFileId>
     {
         /// <summary>
         /// Find a file inside datafile and returns LiteFileInfo instance. Returns null if not found
         /// </summary>
-        LiteFileInfo<T> FindById(T id);
+        LiteFileInfo<TFileId> FindById(TFileId id);
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        IEnumerable<LiteFileInfo<T>> Find(BsonExpression predicate);
+        IEnumerable<LiteFileInfo<TFileId>> Find(BsonExpression predicate);
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        IEnumerable<LiteFileInfo<T>> Find(string predicate, BsonDocument parameters);
+        IEnumerable<LiteFileInfo<TFileId>> Find(string predicate, BsonDocument parameters);
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        IEnumerable<LiteFileInfo<T>> Find(string predicate, params BsonValue[] args);
+        IEnumerable<LiteFileInfo<TFileId>> Find(string predicate, params BsonValue[] args);
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        IEnumerable<LiteFileInfo<T>> Find(Expression<Func<LiteFileInfo<T>, bool>> predicate);
+        IEnumerable<LiteFileInfo<TFileId>> Find(Expression<Func<LiteFileInfo<TFileId>, bool>> predicate);
 
         /// <summary>
         /// Find all files inside file collections
         /// </summary>
-        IEnumerable<LiteFileInfo<T>> FindAll();
+        IEnumerable<LiteFileInfo<TFileId>> FindAll();
 
         /// <summary>
         /// Returns if a file exisits in database
         /// </summary>
-        bool Exists(T id);
+        bool Exists(TFileId id);
 
         /// <summary>
         /// Open/Create new file storage and returns linked Stream to write operations.
         /// </summary>
-        LiteFileStream<T> OpenWrite(T id, string filename, BsonDocument metadata = null);
+        LiteFileStream<TFileId> OpenWrite(TFileId id, string filename, BsonDocument metadata = null);
 
         /// <summary>
         /// Upload a file based on stream data
         /// </summary>
-        LiteFileInfo<T> Upload(T id, string filename, Stream stream, BsonDocument metadata = null);
+        LiteFileInfo<TFileId> Upload(TFileId id, string filename, Stream stream, BsonDocument metadata = null);
 
         /// <summary>
         /// Upload a file based on file system data
         /// </summary>
-        LiteFileInfo<T> Upload(T id, string filename);
+        LiteFileInfo<TFileId> Upload(TFileId id, string filename);
 
         /// <summary>
         /// Update metadata on a file. File must exist.
         /// </summary>
-        bool SetMetadata(T id, BsonDocument metadata);
+        bool SetMetadata(TFileId id, BsonDocument metadata);
 
         /// <summary>
         /// Load data inside storage and returns as Stream
         /// </summary>
-        LiteFileStream<T> OpenRead(T id);
+        LiteFileStream<TFileId> OpenRead(TFileId id);
 
         /// <summary>
         /// Copy all file content to a steam
         /// </summary>
-        LiteFileInfo<T> Download(T id, Stream stream);
+        LiteFileInfo<TFileId> Download(TFileId id, Stream stream);
 
         /// <summary>
         /// Copy all file content to a file
         /// </summary>
-        LiteFileInfo<T> Download(T id, string filename, bool overwritten);
+        LiteFileInfo<TFileId> Download(TFileId id, string filename, bool overwritten);
 
         /// <summary>
         /// Delete a file inside datafile and all metadata related
         /// </summary>
-        bool Delete(T id);
+        bool Delete(TFileId id);
     }
 }
--- LiteDB/Client/Storage/LiteStorage.cs
+++ LiteDB/Client/Storage/LiteStorage.cs
@@ -10,16 +10,16 @@ namespace LiteDB
     /// <summary>
     /// Storage is a special collection to store files and streams.
     /// </summary>
-    public class LiteStorage<T> : ILiteStorage<T>
+    public class LiteStorage<TFileId> : ILiteStorage<TFileId>
     {
         private readonly ILiteDatabase _db;
-        private readonly ILiteCollection<LiteFileInfo<T>> _files;
+        private readonly ILiteCollection<LiteFileInfo<TFileId>> _files;
         private readonly ILiteCollection<BsonDocument> _chunks;
 
         public LiteStorage(ILiteDatabase db, string filesCollection, string chunksCollection)
         {
             _db = db;
-            _files = db.GetCollection<LiteFileInfo<T>>(filesCollection);
+            _files = db.GetCollection<LiteFileInfo<TFileId>>(filesCollection);
             _chunks = db.GetCollection(chunksCollection);
         }
 
@@ -28,11 +28,11 @@ namespace LiteDB
         /// <summary>
         /// Find a file inside datafile and returns LiteFileInfo instance. Returns null if not found
         /// </summary>
-        public LiteFileInfo<T> FindById(T id)
+        public LiteFileInfo<TFileId> FindById(TFileId id)
         {
             if (id == null) throw new ArgumentNullException(nameof(id));
 
-            var fileId = _db.Mapper.Serialize(typeof(T), id);
+            var fileId = _db.Mapper.Serialize(typeof(TFileId), id);
 
             var file = _files.FindById(fileId);
 
@@ -46,7 +46,7 @@ namespace LiteDB
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        public IEnumerable<LiteFileInfo<T>> Find(BsonExpression predicate)
+        public IEnumerable<LiteFileInfo<TFileId>> Find(BsonExpression predicate)
         {
             var files = _files.Query()
                 .Where(predicate)
@@ -54,7 +54,7 @@ namespace LiteDB
 
             foreach (var file in files)
             {
-                var fileId = _db.Mapper.Serialize(typeof(T), file.Id);
+                var fileId = _db.Mapper.Serialize(typeof(TFileId), file.Id);
 
                 file.SetReference(fileId, _files, _chunks);
 
@@ -65,31 +65,31 @@ namespace LiteDB
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        public IEnumerable<LiteFileInfo<T>> Find(string predicate, BsonDocument parameters) => this.Find(BsonExpression.Create(predicate, parameters));
+        public IEnumerable<LiteFileInfo<TFileId>> Find(string predicate, BsonDocument parameters) => this.Find(BsonExpression.Create(predicate, parameters));
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        public IEnumerable<LiteFileInfo<T>> Find(string predicate, params BsonValue[] args) => this.Find(BsonExpression.Create(predicate, args));
+        public IEnumerable<LiteFileInfo<TFileId>> Find(string predicate, params BsonValue[] args) => this.Find(BsonExpression.Create(predicate, args));
 
         /// <summary>
         /// Find all files that match with predicate expression.
         /// </summary>
-        public IEnumerable<LiteFileInfo<T>> Find(Expression<Func<LiteFileInfo<T>, bool>> predicate) => this.Find(_db.Mapper.GetExpression(predicate));
+        public IEnumerable<LiteFileInfo<TFileId>> Find(Expression<Func<LiteFileInfo<TFileId>, bool>> predicate) => this.Find(_db.Mapper.GetExpression(predicate));
 
         /// <summary>
         /// Find all files inside file collections
         /// </summary>
-        public IEnumerable<LiteFileInfo<T>> FindAll() => this.Find((BsonExpression)null);
+        public IEnumerable<LiteFileInfo<TFileId>> FindAll() => this.Find((BsonExpression)null);
 
         /// <summary>
         /// Returns if a file exisits in database
         /// </summary>
-        public bool Exists(T id)
+        public bool Exists(TFileId id)
         {
             if (id == null) throw new ArgumentNullException(nameof(id));
 
-            var fileId = _db.Mapper.Serialize(typeof(T), id);
+            var fileId = _db.Mapper.Serialize(typeof(TFileId), id);
 
             return _files.Exists("_id = @0", fileId);
         }
@@ -101,17 +101,17 @@ namespace LiteDB
         /// <summary>
         /// Open/Create new file storage and returns linked Stream to write operations.
         /// </summary>
-        public LiteFileStream<T> OpenWrite(T id, string filename, BsonDocument metadata = null)
+        public LiteFileStream<TFileId> OpenWrite(TFileId id, string filename, BsonDocument metadata = null)
         {
             // get _id as BsonValue
-            var fileId = _db.Mapper.Serialize(typeof(T), id);
+            var fileId = _db.Mapper.Serialize(typeof(TFileId), id);
 
             // checks if file exists
             var file = this.FindById(id);
 
             if (file == null)
             {
-                file = new LiteFileInfo<T>
+                file = new LiteFileInfo<TFileId>
                 {
                     Id = id,
                     Filename = Path.GetFileName(filename),
@@ -136,7 +136,7 @@ namespace LiteDB
         /// <summary>
         /// Upload a file based on stream data
         /// </summary>
-        public LiteFileInfo<T> Upload(T id, string filename, Stream stream, BsonDocument metadata = null)
+        public LiteFileInfo<TFileId> Upload(TFileId id, string filename, Stream stream, BsonDocument metadata = null)
         {
             using (var writer = this.OpenWrite(id, filename, metadata))
             {
@@ -149,7 +149,7 @@ namespace LiteDB
         /// <summary>
         /// Upload a file based on file system data
         /// </summary>
-        public LiteFileInfo<T> Upload(T id, string filename)
+        public LiteFileInfo<TFileId> Upload(TFileId id, string filename)
         {
             if (filename.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(filename));
 
@@ -162,7 +162,7 @@ namespace LiteDB
         /// <summary>
         /// Update metadata on a file. File must exist.
         /// </summary>
-        public bool SetMetadata(T id, BsonDocument metadata)
+        public bool SetMetadata(TFileId id, BsonDocument metadata)
         {
             var file = this.FindById(id);
 
@@ -182,7 +182,7 @@ namespace LiteDB
         /// <summary>
         /// Load data inside storage and returns as Stream
         /// </summary>
-        public LiteFileStream<T> OpenRead(T id)
+        public LiteFileStream<TFileId> OpenRead(TFileId id)
         {
             var file = this.FindById(id);
 
@@ -194,7 +194,7 @@ namespace LiteDB
         /// <summary>
         /// Copy all file content to a steam
         /// </summary>
-        public LiteFileInfo<T> Download(T id, Stream stream)
+        public LiteFileInfo<TFileId> Download(TFileId id, Stream stream)
         {
             var file = this.FindById(id) ?? throw LiteException.FileNotFound(id.ToString());
 
@@ -206,7 +206,7 @@ namespace LiteDB
         /// <summary>
         /// Copy all file content to a file
         /// </summary>
-        public LiteFileInfo<T> Download(T id, string filename, bool overwritten)
+        public LiteFileInfo<TFileId> Download(TFileId id, string filename, bool overwritten)
         {
             var file = this.FindById(id) ?? throw LiteException.FileNotFound(id.ToString());
 
@@ -222,12 +222,12 @@ namespace LiteDB
         /// <summary>
         /// Delete a file inside datafile and all metadata related
         /// </summary>
-        public bool Delete(T id)
+        public bool Delete(TFileId id)
         {
             if (id == null) throw new ArgumentNullException(nameof(id));
 
             // get Id as BsonValue
-            var fileId = _db.Mapper.Serialize(typeof(T), id);
+            var fileId = _db.Mapper.Serialize(typeof(TFileId), id);
 
             // remove file reference
             var deleted = _files.Delete(fileId);
