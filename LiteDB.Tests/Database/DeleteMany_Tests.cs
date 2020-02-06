01fbaf97bec44768a72f4832508ab0c8d4017364
mbdavid <mauricio.david@gmail.com>
2020-02-06 11:05:47 -0300

    Bugfix in #1445

---
+++ LiteDB.Tests/Database/DeleteMany_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LiteDB;
+using FluentAssertions;
+using Xunit;
+
+namespace LiteDB.Tests.Database
+{
+    public class DeleteMany_Tests
+    {
+        [Fact]
+        public void DeleteMany_With_Arguments()
+        {
+            using (var db = new LiteDatabase(":memory:"))
+            {
+                var c1 = db.GetCollection("Test");
+
+                var d1 = new BsonDocument() { ["_id"] = 1, ["p1"] = 1 };
+                c1.Insert(d1);
+
+                c1.Count().Should().Be(1);
+
+                // try BsonExpression predicate with argument - not deleted
+                var e1 = BsonExpression.Create("$._id = @0", 1);
+                var r1 = c1.DeleteMany(e1);
+
+                r1.Should().Be(1);
+
+                // the same BsonExpression predicate works fine in FindOne
+                var r = c1.FindOne(e1);
+
+            }
+        }
+    }
+}
