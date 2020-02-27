775e23309650fb50b136c50f673b91f68e508280
Jens Schadron <jensschadron@hotmail.com>
2020-02-27 22:17:13 +0100

    Added checkpoint and rebuild to benchmark method and iterationcleanup

--- LiteDB.Benchmarks/Benchmarks/Insertion/MultiThreadedInsertionBenchmark.cs
+++ LiteDB.Benchmarks/Benchmarks/Insertion/MultiThreadedInsertionBenchmark.cs
@@ -8,89 +8,93 @@ using LiteDB.Benchmarks.Models.Generators;
 
 namespace LiteDB.Benchmarks.Benchmarks.Insertion
 {
-    [BenchmarkCategory(Constants.Categories.INSERTION)]
-    public class MultiThreadedInsertionBenchmark
-    {
-        // Insertion data size
-        [Params(10, 50, 100, 500, 1000, 5000 , 10000)]
-        public int DatasetSize;
+	[BenchmarkCategory(Constants.Categories.INSERTION)]
+	public class MultiThreadedInsertionBenchmark
+	{
+		// Insertion data size
+		[Params(10, 50, 100, 500, 1000, 5000, 10000)]
+		public int DatasetSize;
 
-        [Params(null, "SecurePassword")]
-        public string Password;
+		[Params(null, "SecurePassword")]
+		public string Password;
 
-        [Params(1, 2, 5, 10, 20)]
-        public int AmountOfThreads;
+		[Params(1, 2, 5, 10, 20)]
+		public int AmountOfThreads;
 
-        private List<FileMetaBase> _data;
+		private List<FileMetaBase> _data;
 
-        private string DatabasePath => Constants.DATABASE_NAME;
-        private LiteDatabase DatabaseInstance { get; set; }
+		private string DatabasePath => Constants.DATABASE_NAME;
+		private LiteDatabase DatabaseInstance { get; set; }
 
-        [GlobalSetup]
-        public void GlobalSetup()
-        {
-            File.Delete(DatabasePath);
+		[GlobalSetup]
+		public void GlobalSetup()
+		{
+			File.Delete(DatabasePath);
 
-            DatabaseInstance = new LiteDatabase(new ConnectionString(DatabasePath)
-            {
-                Connection = ConnectionType.Direct,
-                Password = Password
-            });
+			DatabaseInstance = new LiteDatabase(new ConnectionString(DatabasePath)
+			{
+				Connection = ConnectionType.Direct,
+				Password = Password
+			});
 
-            // Attempt to initialize the engine before benchmarks start running
-            DatabaseInstance.GetCollectionNames();
+			// Attempt to initialize the engine before benchmarks start running
+			DatabaseInstance.GetCollectionNames();
 
-            _data = FileMetaGenerator<FileMetaBase>.GenerateList(DatasetSize); // executed once for each benchmark run
-        }
+			_data = FileMetaGenerator<FileMetaBase>.GenerateList(DatasetSize); // executed once for each benchmark run
+		}
 
-        private Action[] _insertionTasksArray;
+		private Action[] _insertionTasksArray;
 
-        [IterationSetup]
-        public void IterationSetupGeneric()
-        {
-            _insertionTasksArray = new Action[AmountOfThreads];
-            for (var i = 0; i < AmountOfThreads; i++)
-            {
-                var collectionName = ((char) (65 + i)).ToString();
-                _insertionTasksArray[i] = () => DatabaseInstance.GetCollection<FileMetaBase>(collectionName).Insert(_data);
-            }
-        }
+		[IterationSetup]
+		public void IterationSetupGeneric()
+		{
+			_insertionTasksArray = new Action[AmountOfThreads];
+			for (var i = 0; i < AmountOfThreads; i++)
+			{
+				var collectionName = ((char) (65 + i)).ToString();
+				_insertionTasksArray[i] = () => DatabaseInstance.GetCollection<FileMetaBase>(collectionName).Insert(_data);
+			}
+		}
 
-        [Benchmark]
-        public void InsertionYThreads()
-        {
-            Parallel.Invoke(_insertionTasksArray);
-        }
+		[Benchmark]
+		public void InsertionYThreads()
+		{
+			Parallel.Invoke(_insertionTasksArray);
+			DatabaseInstance.Checkpoint();
+		}
 
-        [IterationCleanup]
-        public void IterationCleanup()
-        {
-            for (var i = 0; i < _insertionTasksArray.Length; i++)
-            {
-                _insertionTasksArray[i] = null;
-            }
+		[IterationCleanup]
+		public void IterationCleanup()
+		{
+			for (var i = 0; i < _insertionTasksArray.Length; i++)
+			{
+				_insertionTasksArray[i] = null;
+			}
 
-            _insertionTasksArray = null;
+			_insertionTasksArray = null;
 
-            foreach (var collectionName in DatabaseInstance.GetCollectionNames())
-            {
-                DatabaseInstance.DropCollection(collectionName);
-            }
-        }
+			foreach (var collectionName in DatabaseInstance.GetCollectionNames())
+			{
+				DatabaseInstance.DropCollection(collectionName);
+			}
 
-        [GlobalCleanup]
-        public void GlobalCleanup()
-        {
-            foreach (var collectionName in DatabaseInstance.GetCollectionNames())
-            {
-                DatabaseInstance.DropCollection(collectionName);
-            }
+			DatabaseInstance.Checkpoint();
+			DatabaseInstance.Rebuild();
+		}
 
-            DatabaseInstance.Checkpoint();
-            DatabaseInstance?.Dispose();
-            DatabaseInstance = null;
+		[GlobalCleanup]
+		public void GlobalCleanup()
+		{
+			foreach (var collectionName in DatabaseInstance.GetCollectionNames())
+			{
+				DatabaseInstance.DropCollection(collectionName);
+			}
 
-            File.Delete(DatabasePath);
-        }
-    }
+			DatabaseInstance.Checkpoint();
+			DatabaseInstance?.Dispose();
+			DatabaseInstance = null;
+
+			File.Delete(DatabasePath);
+		}
+	}
 }
