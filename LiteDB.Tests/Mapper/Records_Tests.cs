8ab7b7a904d91999a586fa3706e5259a8fc1e021
Mauricio David <mauricio.david@gmail.com>
2022-12-24 17:21:55 -0300

    Adding Record BsonMapper tests and upgrade to .net 6

---
+++ LiteDB.Tests/Mapper/Records_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace LiteDB.Tests.Mapper
+{
+    public class Records_Tests
+    {
+        public record User(int Id, string Name);
+
+
+        [Fact]
+        public void Record_Simple_Mapper()
+        {
+            var mapper = new BsonMapper();
+
+            var user = new User(1, "John");
+            var doc = mapper.ToDocument(user);
+            var user2 = mapper.ToObject<User>(doc);
+
+            Assert.Equal(user.Id, user2.Id);
+            Assert.Equal(user.Name, user2.Name);
+        }
+    }
+}
