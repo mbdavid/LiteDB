4bd59c11498a4c717779c68bf81ce283e87dc2c9
mbdavid <mauricio.david@gmail.com>
2023-11-22 12:12:58 -0300

    clean up before continue

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,80 +1,80 @@
-﻿//// SETUP //////////////////
-//const string VER = "v6";
-//var INSERT_1 = new Range(1, 100_000);
-//var DELETE_1 = new Range(1, 40_000);
-//var INSERT_2 = new Range(1, 30_000);
-//////////////////////////
+﻿// SETUP //////////////////
+const string VER = "v6";
+var INSERT_1 = new Range(1, 100_000);
+var DELETE_1 = new Range(1, 40_000);
+var INSERT_2 = new Range(1, 30_000);
+////////////////////////
 
-//// DATASETS
-//var insert1 = GetData(INSERT_1, 100, 300).ToArray();
-//var insert2 = GetData(INSERT_2, 5, 10).ToArray();
+// DATASETS
+var insert1 = GetData(INSERT_1, 100, 300).ToArray();
+var insert2 = GetData(INSERT_2, 5, 10).ToArray();
 
-//var delete1 = Enumerable.Range(DELETE_1.Start.Value, DELETE_1.End.Value).Select(x => new BsonInt32(x)).ToArray();
+var delete1 = Enumerable.Range(DELETE_1.Start.Value, DELETE_1.End.Value).Select(x => new BsonInt32(x)).ToArray();
 
-//// INITIALIZE
-//var filename = @$"C:\LiteDB\temp\test-{VER}-{DateTime.Now.Ticks}.db";
-//var settings = new EngineSettings { Filename = filename };
-//var db = new LiteEngine(settings);
+// INITIALIZE
+var filename = @$"C:\LiteDB\temp\test-{VER}-{DateTime.Now.Ticks}.db";
+var settings = new EngineSettings { Filename = filename };
+var db = new LiteEngine(settings);
 
-//// OPEN
-//await db.OpenAsync();
+// OPEN
+await db.OpenAsync();
 
-///*
-//var doc = new BsonDocument
-//{
-//    ["_id"] = i,
-//    ["name"] = Faker.Fullname(),
-//    ["age"] = Faker.Age(),
-//    ["created"] = Faker.Birthday(),
-//    ["country"] = BsonDocument.DbRef(Faker.Next(1, 10), "col2"),
-//    ["lorem"] = Faker.Lorem(lorem, loremEnd)
-//};
-//*/
+/*
+var doc = new BsonDocument
+{
+    ["_id"] = i,
+    ["name"] = Faker.Fullname(),
+    ["age"] = Faker.Age(),
+    ["created"] = Faker.Birthday(),
+    ["country"] = BsonDocument.DbRef(Faker.Next(1, 10), "col2"),
+    ["lorem"] = Faker.Lorem(lorem, loremEnd)
+};
+*/
 
-//// RUN 
+// RUN 
 
-//await db.RunAsync($"Set USER_VERSION", "PRAGMA USER_VERSION = 25");
+await db.RunAsync($"Set USER_VERSION", "PRAGMA USER_VERSION = 25");
 
-//await db.RunAsync($"Create Collection 'col1'", "CREATE COLLECTION col1");
+await db.RunAsync($"Create Collection 'col1'", "CREATE COLLECTION col1");
 
-//await db.RunAsync($"Insert col1 {insert1.Length:n0}", "INSERT INTO col1 VALUES @0", insert1);
+await db.RunAsync($"Insert col1 {insert1.Length:n0}", "INSERT INTO col1 VALUES @0", insert1);
 
-//await db.RunAsync($"Create Index", "CREATE INDEX idx_age ON col1 ($.age)", insert1);
+await db.RunAsync($"Create Index", "CREATE INDEX idx_age ON col1 ($.age)", insert1);
 
-//await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM col1 WHERE age = 32");
+await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM col1 WHERE age = 32");
 
-//await db.RunAsync($"Rename Collection", "RENAME COLLECTION col1 TO new_col");
+await db.RunAsync($"Rename Collection", "RENAME COLLECTION col1 TO new_col");
 
-//await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) FROM new_col WHERE age = 32");
+await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) FROM new_col WHERE age = 32");
 
-//await db.RunAsync($"Delete age = 32", "DELETE new_col WHERE age = 32");
+await db.RunAsync($"Delete age = 32", "DELETE new_col WHERE age = 32");
 
-//await db.RunQueryAsync($"Query1", @"EXPLAIN SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
+await db.RunQueryAsync($"Query1", @"EXPLAIN SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
 
-//await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
+await db.RunQueryAsync(10, $"Query1", @"SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
 
-//await db.RunAsync($"Drop Index", "DROP INDEX new_col.idx_age");
+await db.RunAsync($"Drop Index", "DROP INDEX new_col.idx_age");
 
-//await db.RunQueryAsync(10, $"Query1", @"EXPLAIN SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
+await db.RunQueryAsync(10, $"Query1", @"EXPLAIN SELECT COUNT(_id) contador, contador + 1000 'contador_mais_mil' FROM new_col WHERE age = 32");
 
-//await db.RunAsync($"Checkpoint", "CHECKPOINT");
+await db.RunAsync($"Checkpoint", "CHECKPOINT");
 
-//await db.RunAsync($"Drop Collection", "DROP COLLECTION new_col");
+await db.RunAsync($"Drop Collection", "DROP COLLECTION new_col");
 
 
-////db.Dump();
+//db.Dump();
 
-//// SHUTDOWN
-//await db.ShutdownAsync();
-//db.Dispose();
+// SHUTDOWN
+await db.ShutdownAsync();
+db.Dispose();
 
-//// PRINT
-//Console.WriteLine();
-//Profiler.PrintResults(filename);
+// PRINT
+Console.WriteLine();
+Profiler.PrintResults(filename);
 
-//#if DEBUG
-//Console.WriteLine($"# DEBUG - {VER}");
-//#else
-//Console.WriteLine($"# RELEASE - {VER}");
-//#endif
-////Console.ReadKey();
+#if DEBUG
+Console.WriteLine($"# DEBUG - {VER}");
+#else
+Console.WriteLine($"# RELEASE - {VER}");
+#endif
+//Console.ReadKey();
