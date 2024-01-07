254ee8b0a434ee2d7a5bebd83d1e4fcf7309cdc0
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-06 21:43:03 -0300

    removed unused code

--- src/LiteDB.Generator/_Imports.cs
+++ src/LiteDB.Generator/_Imports.cs
@@ -8,5 +8,3 @@ global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using Microsoft.CodeAnalysis.Text;
-
-global using static SourceGenDebugger;
