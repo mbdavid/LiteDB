1061987feb13715281e395ab3748844782818c65
Sasha-hi <Sasha-hi@mail.ru>
2024-02-26 21:31:14 +0200

    Fixed memory leak due to infinite static caches
    Use MemoryCache with fixed SlidingExpiration.
    Increasing the required framework version to 4.5.1
    
    #2421

--- LiteDB/Document/Expression/BsonExpression.cs
+++ LiteDB/Document/Expression/BsonExpression.cs
@@ -8,6 +8,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Caching.Memory;
 using static LiteDB.Constants;
 
 namespace LiteDB
@@ -279,8 +280,8 @@ namespace LiteDB
 
         #region Static method
 
-        private static readonly ConcurrentDictionary<string, BsonExpressionEnumerableDelegate> _cacheEnumerable = new ConcurrentDictionary<string, BsonExpressionEnumerableDelegate>();
-        private static readonly ConcurrentDictionary<string, BsonExpressionScalarDelegate> _cacheScalar = new ConcurrentDictionary<string, BsonExpressionScalarDelegate>();
+        private static readonly IMemoryCache _cacheEnumerable = new MemoryCache(new MemoryCacheOptions());
+        private static readonly IMemoryCache _cacheScalar = new MemoryCache(new MemoryCacheOptions());
 
         /// <summary>
         /// Parse string and create new instance of BsonExpression - can be cached
@@ -358,9 +359,11 @@ namespace LiteDB
             // in both case, try use cached compiled version
             if (expr.IsScalar)
             {
-                var cached = _cacheScalar.GetOrAdd(expr.Source, s =>
+                var cached = _cacheScalar.GetOrCreate(expr.Source, cacheEntry =>
                 {
-                    var lambda = System.Linq.Expressions.Expression.Lambda<BsonExpressionScalarDelegate>(expr.Expression, context.Source, context.Root, context.Current, context.Collation, context.Parameters);
+                    cacheEntry.SlidingExpiration = TimeSpan.FromMinutes(1);
+
+                    var lambda = Expression.Lambda<BsonExpressionScalarDelegate>(expr.Expression, context.Source, context.Root, context.Current, context.Collation, context.Parameters);
 
                     return lambda.Compile();
                 });
@@ -369,9 +372,11 @@ namespace LiteDB
             }
             else
             {
-                var cached = _cacheEnumerable.GetOrAdd(expr.Source, s =>
+                var cached = _cacheEnumerable.GetOrCreate(expr.Source, cacheEntry =>
                 {
-                    var lambda = System.Linq.Expressions.Expression.Lambda<BsonExpressionEnumerableDelegate>(expr.Expression, context.Source, context.Root, context.Current, context.Collation, context.Parameters);
+                    cacheEntry.SlidingExpiration = TimeSpan.FromMinutes(1);
+
+                    var lambda = Expression.Lambda<BsonExpressionEnumerableDelegate>(expr.Expression, context.Source, context.Root, context.Current, context.Collation, context.Parameters);
 
                     return lambda.Compile();
                 });
