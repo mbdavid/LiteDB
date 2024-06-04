74e9ef1cd7d07e39e6966ea0c6e70629add340ca
JKamsker <11245306+JKamsker@users.noreply.github.com>
2024-06-04 18:17:21 +0200

    Add test to assert that the executionPlan query also releases the lock

--- LiteDB.Tests/Issues/Issue2471_Test.cs
+++ LiteDB.Tests/Issues/Issue2471_Test.cs
@@ -46,4 +46,52 @@ public class Issue2471_Test
             collection.FindById(1);
         }
     }
+
+    #region Model
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int[] Phones { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+    }
+
+    #endregion Model
+
+    // Copied from IndexMultiKeyIndex, but this time we ensure that the lock is released by calling  db.Checkpoint()
+    [Fact]
+    public void Ensure_Query_GetPlan_Releases_Lock()
+    {
+        using var db = new LiteDatabase(new MemoryStream());
+        var col = db.GetCollection<User>();
+
+        col.Insert(new User { Name = "John Doe", Phones = new int[] { 1, 3, 5 }, Addresses = new List<Address> { new Address { Street = "Av.1" }, new Address { Street = "Av.3" } } });
+        col.Insert(new User { Name = "Joana Mark", Phones = new int[] { 1, 4 }, Addresses = new List<Address> { new Address { Street = "Av.3" } } });
+
+        // create indexes
+        col.EnsureIndex(x => x.Phones);
+        col.EnsureIndex(x => x.Addresses.Select(z => z.Street));
+
+        // testing indexes expressions
+        var indexes = db.GetCollection("$indexes").FindAll().ToArray();
+
+        indexes[1]["expression"].AsString.Should().Be("$.Phones[*]");
+        indexes[2]["expression"].AsString.Should().Be("MAP($.Addresses[*]=>@.Street)");
+
+        // doing Phone query
+        var queryPhone = col.Query()
+            .Where(x => x.Phones.Contains(3));
+
+        var planPhone = queryPhone.GetPlan();
+
+        Action act = () => db.Checkpoint();
+
+        act.Should().NotThrow();
+    }
 }
