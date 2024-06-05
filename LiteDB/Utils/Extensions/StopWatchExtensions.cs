2137a78d382e9a6e345c77dc45d4b1ef7eb19db6
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-06-05 19:31:54 -0300

    move back to spaces

--- LiteDB/Utils/Extensions/StopWatchExtensions.cs
+++ LiteDB/Utils/Extensions/StopWatchExtensions.cs
@@ -3,37 +3,37 @@ using System.Diagnostics;
 
 namespace LiteDB.Utils.Extensions
 {
-	public static class StopWatchExtensions
-	{
-		/// <summary>
-		/// Start the stopwatch and returns an IDisposable that will stop the stopwatch when disposed
-		/// </summary>
-		/// <param name="stopwatch"><see cref="Stopwatch"/> instance that will be used to measure time.</param>
-		/// <returns></returns>
-		public static DisposableAction StartDisposable(this Stopwatch stopwatch)
-		{
-			stopwatch.Start();
-			return new DisposableAction(stopwatch.Stop);
-		}
+    public static class StopWatchExtensions
+    {
+        /// <summary>
+        /// Start the stopwatch and returns an IDisposable that will stop the stopwatch when disposed
+        /// </summary>
+        /// <param name="stopwatch"><see cref="Stopwatch"/> instance that will be used to measure time.</param>
+        /// <returns></returns>
+        public static DisposableAction StartDisposable(this Stopwatch stopwatch)
+        {
+            stopwatch.Start();
+            return new DisposableAction(stopwatch.Stop);
+        }
 
-		/// <summary>
-		/// This struct isn't mean to be instantiated by users, so its ctor is internal.
-		/// If you want to use an instance of it call <see cref="StartDisposable(Stopwatch)"/> method.
-		/// </summary>
-		public readonly struct DisposableAction : IDisposable
-		{
-			private readonly Action _action;
+        /// <summary>
+        /// This struct isn't mean to be instantiated by users, so its ctor is internal.
+        /// If you want to use an instance of it call <see cref="StartDisposable(Stopwatch)"/> method.
+        /// </summary>
+        public readonly struct DisposableAction : IDisposable
+        {
+            private readonly Action _action;
 
-			internal DisposableAction(Action action)
-			{
-				_action = action;
-			}
+            internal DisposableAction(Action action)
+            {
+                _action = action;
+            }
 
-			public void Dispose()
-			{
-				_action();
-			}
-		}
-	}
+            public void Dispose()
+            {
+                _action();
+            }
+        }
+    }
 
 }
