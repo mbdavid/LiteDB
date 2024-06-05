4208501b44ed9b988ca90e3d5c58239cbfeb6540
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-06-05 19:22:09 -0300

    Add benchmark for the changes

---
+++ LiteDB.Benchmarks/Benchmarks/GeneralMethods/StopwatchExtensionsBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using LiteDB.Utils.Extensions;
+using System;
+using System.Diagnostics;
+
+namespace LiteDB.Benchmarks.Benchmarks.GeneralMethods
+{
+	[MemoryDiagnoser]
+	[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+	[SimpleJob(RuntimeMoniker.Net60)]
+	public class StopwatchExtensionsBenchmark
+	{
+
+		Stopwatch stopwatch = new Stopwatch();
+		Action a = () => { };
+		[Benchmark]
+		public StopWatchExtensions.DisposableAction ReturningNewDisposableAction()
+		{
+			return stopwatch.StartDisposable();
+		}
+
+
+		//[Benchmark]
+		//public IDisposable ReturningasIDisposableAction()
+		//{
+		//	return stopwatch.StartDisposable();
+		//}
+
+		//[Benchmark]
+		//public async Task<int> ConsumingTheDisposable()
+		//{
+		//	using var test = stopwatch.StartDisposable();
+		//	await Task.Yield();
+
+		//	return 42;
+		//}
+	}
+}
