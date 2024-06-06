1c8c791fab8d60fac440d47a597e05243dc84bd8
JKamsker <11245306+JKamsker@users.noreply.github.com>
2024-06-06 08:34:37 +0200

    Add failing tests

---
+++ LiteDB.Tests/Failing/Issue2475_tests.cs
@@ -0,0 +1,41 @@
+﻿using LiteDB.Tests.Utils;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xunit;
+
+namespace LiteDB.Tests.Issues;
+
+/// <summary>
+/// The issue here is that litedb does not support complex linq queries (``.Where(x => x.PhoneBooks.Any(x => ids.Contains(x.Id)))``)
+/// </summary>
+public class Issue2475_tests
+{
+    private class PhoneBookCategory
+    {
+        public Guid Id { get; set; }
+
+        [BsonRef("PhoneBook")]
+        public List<PhoneBook> PhoneBooks { get; set; }
+    }
+
+    private class PhoneBook
+    {
+        public Guid Id { get; set; }
+    }
+
+    [FailingFact]
+    public void Supports_Complex_Queries()
+    {
+        List<Guid> ids = new() { Guid.NewGuid(), Guid.NewGuid() };
+
+        using var db = new LiteDatabase(":memory:");
+        var collection = db.GetCollection<PhoneBookCategory>("data");
+
+        var set = collection.Query()
+            .Where(x => x.PhoneBooks.Any(x => ids.Contains(x.Id)))
+            .ToList();
+    }
+}
---
+++ LiteDB.Tests/Utils/FailingFactAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace LiteDB.Tests.Utils;
+
+internal class FailingFactAttribute : FactAttribute
+{
+    public FailingFactAttribute(string reason = "The test demonstrates a bug that currently exists")
+    {
+        Skip = reason;
+    }
+}
