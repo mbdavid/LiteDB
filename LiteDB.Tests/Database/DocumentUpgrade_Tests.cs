02e23d92361a0eb1817f1f11c03a096d5cda5679
JKamsker <jonas@kamsker.at>
2024-06-07 00:52:58 +0200

    Add deserialization hook to bsonmapper

--- LiteDB.Tests/Database/DocumentUpgrade_Tests.cs
+++ LiteDB.Tests/Database/DocumentUpgrade_Tests.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 
 using LiteDB.Engine;
 
@@ -38,10 +38,24 @@ public class DocumentUpgrade_Tests
 
         ms.Position = 0;
 
-        var engine = new LiteEngine(new EngineSettings
+        using var engine = new LiteEngine(new EngineSettings
         {
             DataStream = ms,
-            ReadTransform = ReadTransform
+            ReadTransform = (collectionName, val) =>
+            {
+                if (val is not BsonDocument doc)
+                {
+                    return val;
+                }
+
+                if (doc.TryGetValue("version", out var version) && version.AsInt32 == 1)
+                {
+                    doc["version"] = 2;
+                    doc["age"] = 30;
+                }
+
+                return val;
+            }
         });
 
         using (var db = new LiteDatabase(engine))
@@ -58,19 +72,62 @@ public class DocumentUpgrade_Tests
         }
     }
 
-    private BsonValue ReadTransform(string arg1, BsonValue val)
+    [Fact]
+    public void DocumentUpgrade_BsonMapper_Test()
     {
-        if (!(val is BsonDocument bdoc))
+        var ms = new MemoryStream();
+        using (var db = new LiteDatabase(ms))
         {
-            return val;
+            var col = db.GetCollection("col");
+
+            col.Insert(new BsonDocument { ["version"] = 1, ["_id"] = 1, ["name"] = "John" });
         }
 
-        if (bdoc.TryGetValue("version", out var version) && version.AsInt32 == 1)
+        ms.Position = 0;
+
+        using (var db = new LiteDatabase(ms))
         {
-            bdoc["version"] = 2;
-            bdoc["age"] = 30;
+            var col = db.GetCollection("col");
+
+            col.Count().Should().Be(1);
+
+            var doc = col.FindById(1);
+
+            doc["version"].AsInt32.Should().Be(1);
+            doc["name"].AsString.Should().Be("John");
+            doc["age"].AsInt32.Should().Be(0);
         }
 
-        return val;
+        ms.Position = 0;
+
+        var mapper = new BsonMapper();
+        mapper.OnDeserialization = (sender, type, val) =>
+        {
+            if (val is not BsonDocument doc)
+            {
+                return val;
+            }
+
+            if (doc.TryGetValue("version", out var version) && version.AsInt32 == 1)
+            {
+                doc["version"] = 2;
+                doc["age"] = 30;
+            }
+
+            return doc;
+        };
+
+        using (var db = new LiteDatabase(ms, mapper))
+        {
+            var col = db.GetCollection("col");
+
+            col.Count().Should().Be(1);
+
+            var doc = col.FindById(1);
+
+            doc["version"].AsInt32.Should().Be(2);
+            doc["name"].AsString.Should().Be("John");
+            doc["age"].AsInt32.Should().Be(30);
+        }
     }
 }
