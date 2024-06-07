48565c3e82e47abad51a1a4c6f9111bbe743fd85
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2024-06-08 00:16:38 +0200

    Add tests to validate that encrypted dbs migrate

---
+++ LiteDB.Tests/Issues/Issue2494_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace LiteDB.Tests.Issues;
+
+public class Issue2494_Tests
+{
+    [Fact]
+    public static void Test()
+    {
+        var original = "../../../Resources/Issue_2494_EncryptedV4.db";
+        using var filename = new TempFile(original);
+
+        var connectionString = new ConnectionString(filename)
+        {
+            Password = "pass123",
+            Upgrade = true,
+        };
+
+        using (var db = new LiteDatabase(connectionString)) // <= throws as of version 5.0.18
+        {
+            var col = db.GetCollection<PlayerDto>();
+            col.FindAll();
+        }
+    }
+
+    public class PlayerDto
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public PlayerDto(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public PlayerDto()
+        {
+        }
+    }
+}
